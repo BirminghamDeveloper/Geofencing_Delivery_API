0ff72fac426205c0db9958c7603e9dfaa3520d7e
Mustafa <smart1hashi@gmail.com>
2025-04-28 12:09:17 +0100

    % Edited the Map Style and add Zoom + Display instructions Features

--- app/src/main/java/com/hashinology/geofencingapi/ui/maps/MapsFragment.kt
+++ app/src/main/java/com/hashinology/geofencingapi/ui/maps/MapsFragment.kt
@@ -1,11 +1,14 @@
 package com.hashinology.geofencingapi.ui.maps
 
+import android.annotation.SuppressLint
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -13,13 +16,21 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
 import com.hashinology.geofencingapi.R
 import com.hashinology.geofencingapi.databinding.FragmentMapsBinding
+import com.hashinology.geofencingapi.util.ExtensionFunctions.hide
+import com.hashinology.geofencingapi.util.ExtensionFunctions.show
+import com.hashinology.geofencingapi.viewmodels.SharedViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MapsFragment : Fragment(), OnMapReadyCallback {
     private var _binding: FragmentMapsBinding? = null
     private val binding get() = _binding!!
+
+    private val shareedVM: SharedViewModel by activityViewModels()
 
     private lateinit var map: GoogleMap
 
@@ -30,8 +41,12 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
     ): View {
         _binding = FragmentMapsBinding.inflate(inflater, container, false)
 
-        binding.floatingActionButton.setOnClickListener {
+        binding.addGeofenceFab.setOnClickListener {
             findNavController().navigate(R.id.action_mapsFragment_to_add_geofence_graph)
+        }
+
+        binding.geofencesFab.setOnClickListener {
+            findNavController().navigate(R.id.action_mapsFragment_to_geofencesFragment)
         }
 
         return binding.root
@@ -43,11 +58,45 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
         mapFragment?.getMapAsync(this)
     }
 
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap!!
-        val sydney = LatLng(52.4822694,-1.8900078)
-        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        map.setMapStyle(MapStyleOptions.loadRawResourceStyle(requireContext(), R.raw.mapstyle))
+        map.isMyLocationEnabled = true
+        map.uiSettings.apply {
+            isMapToolbarEnabled = false
+            isMyLocationButtonEnabled = true
+        }
+        onGeofenceReady()
+    }
+
+    private fun onGeofenceReady() {
+        if (shareedVM.geofenceReady){
+            shareedVM.geofenceReady = false
+            displayInfoMessage()
+            zoomToSelectedLocation()
+        }
+    }
+
+    private fun displayInfoMessage() {
+        lifecycleScope.launch {
+            binding.infoMessageTextView.show()
+            delay(2000)
+            binding.infoMessageTextView
+                .animate()
+                .alpha(0f)
+                .duration = 800
+            delay(1000)
+            binding.infoMessageTextView.hide()
+        }
+    }
+
+    private fun zoomToSelectedLocation() {
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(shareedVM.geoLatLng, 10f),
+            2000,
+            null
+        )
     }
 
     override fun onDestroy() {
